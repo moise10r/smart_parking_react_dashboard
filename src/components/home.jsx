07a8a06a7f13a54d59751bbdfd1bf6897c8547a6
moise10r <moiserushanika2006@gmail.com>
2021-11-04 14:13:35 +0200

    Add home compenent

---
+++ src/components/home.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+
+
+class Home extends Component {
+
+}
+
+export default Home;
--- src/utils/pagination.js
+++
@@ -1,5 +0,0 @@
-import _ from "lodash"
-export function paginate(items,pageNumber,pageSize) {
-    let startIndex = (pageNumber-1)*pageSize
-    return _(items).slice(startIndex).take(pageSize).value();
-}
