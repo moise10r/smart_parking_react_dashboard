96124c3abedee0a7d5c247c1794c674ff334d3c4
moise10r <moiserushanika2006@gmail.com>
2021-11-04 16:58:33 +0200

    Refractor home component

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,81 +1,80 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { RiArrowRightSLine } from 'react-icons/ri';
 import axios from 'axios';
 
-class Login extends Component {
-	state = {
+const Login = (props) => {
+	const [state, setstate] = useState({
 		allInput: {
 			email: '',
 			password: '',
 		},
 		errorMessage: '',
-	};
-	async	componentDidMount() {
-		
-	}
-	handelChange = ({ currentTarget: input }) => {
-		const allInput = { ...this.state.allInput };
+	});
+
+	const handelChange = ({ currentTarget: input }) => {
+		const allInput = { ...state.allInput };
 		allInput[input.name] = input.value;
-		this.setState({ allInput });
+		setstate({ allInput });
 	};
-	handelSubmit = async (e) => {
+	const handelSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const response = await axios.post('https://smart-parking-management.herokuapp.com/api/login/', this.state.allInput)
-			const user = response.data
-			localStorage.setItem('currentUser', JSON.stringify(user))
-			this.props.history.push(`/home`)
-		} catch(ex) {
-			this.setState({errorMessage: 'Your Email or Passor is incorrect'})
+			const response = await axios.post(
+				'https://smart-parking-management.herokuapp.com/api/login/',
+				state.allInput
+			);
+			const user = response.data;
+			localStorage.setItem('currentUser', JSON.stringify(user));
+			props.history.push(`/home`);
+		} catch (ex) {
+			setstate({ errorMessage: 'Your Email or Passor is incorrect' });
 		}
-	
 	};
-	render() {
-		const { handelChange, handelSubmit } = this;
-		const { email, password } = this.state.allInput;
-		const { errorMessage } = this.state;
-		console.log(errorMessage);
-		return (
-			<div className='register'>
-				<h1>LOGIN</h1>
-				<form onSubmit={handelSubmit}>
-					<label htmlFor='driver'>
-						<p>Email</p>
-						<div className='input-content'>
-							<input
-								id='email'
-								name='email'
-								value={email}
-								type='email'
-								placeholder="Enter your Email"
-								onChange={handelChange}
-								required
-							/>
-						</div>
-					</label>
-					<label htmlFor='email'>
-						<p>Password</p>
-						<div className='input-content'>
-							<input
-								id='password'
-								name='password'
-								type='text'
-								value={password}
-								placeholder='Enter your password'
-								onChange={handelChange}
-							/>
-						</div>
-					</label>
-					<div className='go-to'>
-						<button className='btne'>
-							<RiArrowRightSLine />
-						</button>
+	const { email, password } = state.allInput;
+	const { errorMessage } = state;
+	console.log(errorMessage);
+	return (
+		<div className='register'>
+			<h1>LOGIN</h1>
+			<form onSubmit={handelSubmit}>
+				<label htmlFor='driver'>
+					<p>Email</p>
+					<div className='input-content'>
+						<input
+							id='email'
+							name='email'
+							value={email}
+							type='email'
+							placeholder='Enter your Email'
+							onChange={handelChange}
+							required
+						/>
 					</div>
-				</form>
-				{errorMessage && <p style={{ marginTop: "20px",  color: "red"}}>{errorMessage}</p>}
-			</div>
-		);
-	}
-}
+				</label>
+				<label htmlFor='email'>
+					<p>Password</p>
+					<div className='input-content'>
+						<input
+							id='password'
+							name='password'
+							type='text'
+							value={password}
+							placeholder='Enter your password'
+							onChange={handelChange}
+						/>
+					</div>
+				</label>
+				<div className='go-to'>
+					<button className='btne'>
+						<RiArrowRightSLine />
+					</button>
+				</div>
+			</form>
+			{errorMessage && (
+				<p style={{ marginTop: '20px', color: 'red' }}>{errorMessage}</p>
+			)}
+		</div>
+	);
+};
 
 export default Login;
